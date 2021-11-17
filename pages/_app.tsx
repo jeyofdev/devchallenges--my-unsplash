31b62fa4ec24aaeb1e11b41e70ce68d3eadd7568
jeyofdev <jgregoire.pro@gmail.com>
2021-11-17 12:04:02 +0100

    seo : init tag head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,29 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import PhotosContextProvider from '../context/PhotosContext';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <PhotosContextProvider>
-      <Component {...pageProps} />;
-    </PhotosContextProvider>
+    <>
+      <Head>
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta
+          name="description"
+          content="My solution for the challenge 'My unsplash' of the website https://devchallenges.io/"
+        />
+        <meta
+          name="keywords"
+          content="React, NextJs, challenge, My unsplash, devchallenges"
+        />
+        <link rel="icon" href="/favicon.svg" />
+        <title>My unsplash by jeyofdev</title>
+      </Head>
+      <PhotosContextProvider>
+        <Component {...pageProps} />;
+      </PhotosContextProvider>
+    </>
   );
 }
 
